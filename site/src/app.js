ed9e5146eebed3634d0e140824a2989c23fc5ab8
hi2048 <291882199@qq.com>
2020-02-24 20:27:38 +0800

    update hi2048-utils.

--- site/src/app.js
+++ site/src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './style.css';
+import { random } from 'hi2048-utils';
 import { ReduxDemo } from './redux';
 import { HookDemo } from './hook';
 import { HocDemo } from './hoc';
@@ -8,6 +9,9 @@ function App(props) {
   return (
     <div>
       <div>Hello <span>{ props.name }</span>!</div>
+      <hr />
+
+      <div>utils <span>{ random(100) }</span></div>
       <hr />
 
       <ReduxDemo></ReduxDemo>
